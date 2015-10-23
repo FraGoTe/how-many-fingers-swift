33721dd3b760b280cef35b59ba3f4948ac1bb7cc
Francis Gonzales <fgonzalestello91@gmail.com>
2015-10-23 18:46:05 -0500

    Initial Commit

---
+++ how many fingers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  how many fingers
+//
+//  Created by Francis Gonzales Tello on 10/23/15.
+//  Copyright © 2015 Francis Gonzales Tello. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
